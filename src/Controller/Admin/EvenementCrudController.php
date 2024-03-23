d95617a633bbe3fc03187f470e0e1051bfd595ed
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-03-24 00:27:51 +0100

    MAIN> Configuration des champs de saisies du CRUD Evenement

--- src/Controller/Admin/EvenementCrudController.php
+++ src/Controller/Admin/EvenementCrudController.php
@@ -4,9 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Evenement;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Validator\Constraints\Date;
 
 class EvenementCrudController extends AbstractCrudController
 {
@@ -15,14 +18,15 @@ class EvenementCrudController extends AbstractCrudController
         return Evenement::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+    yield TextField::new('title', 'Titre');
+    yield TextEditorField::new('description', 'Description de l\'évènement');
+    yield NumberField::new('price', 'Prix');
+    yield DateTimeField::new('start', 'Début de l\'évènement');
+    yield DateTimeField::new('end', 'Fin de l\'évènement');
+    yield DateField::new('createAt', 'Date de création de l\'évènement');
     }
-    */
+    
 }
