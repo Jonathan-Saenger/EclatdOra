1e6cca5d65eaebf3c63642875c5457b7f1073441
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-03-24 21:32:54 +0100

    MAIN> Configuration du CRUD Evenement et installation du bundle Intl

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class EvenementController extends AbstractController
 {
     #[Route('/evenement', name: 'app_evenement')]
-    public function index(): Response
+    public function index(EvenementRepository $EvenementRepository): Response
     {
         return $this->render('evenement/evenement.html.twig', [
             'controller_name' => 'EvenementController',
+            'evenements' => $EvenementRepository->findBy([], ['createAt' => 'DESC']) 
         ]);
     }
 }
