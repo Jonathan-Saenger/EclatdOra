c2e4ce22aa015b2cc9665237af857e297546fb8f
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-04-27 21:35:19 +0200

    MAIN> Modification de test pour boite mail

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,12 +55,12 @@ class RegistrationController extends AbstractController
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
-
-            return $userAuthenticator->authenticateUser(
+            return $this->redirectToRoute('app_home');
+           /* return $userAuthenticator->authenticateUser(
                 $user,
                 $authenticator,
                 $request
-            );
+            );*/
         }
 
         return $this->render('registration/register.html.twig', [
