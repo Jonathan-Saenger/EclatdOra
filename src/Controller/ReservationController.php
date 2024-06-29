4007ad495f809782b2e6f020f942abe8fa567138
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-06-29 22:59:01 +0200

    F10> Reservation: Configuration de fichier dynamique TWIG pour la réservation et de la page Hypnose

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -3,19 +3,15 @@
 namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Extension\HttpFoundationExtension;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\Reservation;
-use App\Form\ReservationType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Mailer\MailerInterface;
 use App\Form\EmailInscriptionType;
 use App\Entity\EmailInscription;
 use App\Repository\ReservationRepository;
-use App\Form\ReservationChoiceType;
 
 class ReservationController extends AbstractController
 {
@@ -79,14 +75,34 @@ class ReservationController extends AbstractController
             return $this->redirectToRoute('app_home');
         }
         // Récupérez les réservations pour la prestation donnée
-        $reservations = $reservationRepository->findBy(['prestation' => $prestation]);
+        $reservation = $reservationRepository->findBy(['prestation' => $prestation], ['date' => 'ASC']);
 
         return $this->render('reservation/reservation.html.twig', [
-            'reservations' => $reservations,
+            'reservation' => $reservation,
             'prestation' => $prestation,
             'formEmail' => $formEmail->createView(),
         ]);
     }
+
+    // Validation du formulaire : le compléter avec l'envoi du formulaire vers Brevo
+    
+    //#[Route('/submit-reservation', name: 'route_name_to_handle_form_submission')]
+    //public function handleFormSubmission(Request $request, ReservationRepository $reservationRepository, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
+    //{
+    //    $reservationId = $request->request->get('reservation');
+    //   if ($reservationId) {
+    //       $reservation = $reservationRepository->find($reservationId);
+
+            // Logic to handle the selected reservation
+            // For example, you can send an email, save to database, etc.
+
+    //       $this->addFlash('success', 'Votre réservation a été prise en compte pour le créneau sélectionné.');
+    //       return $this->redirectToRoute('some_route_after_submission');
+    //   }
+
+    //   $this->addFlash('error', 'Veuillez sélectionner un créneau.');
+    //   return $this->redirectToRoute('current_route_with_form'); // Replace with the route name where the form is displayed
+    //}
 }
 
 
