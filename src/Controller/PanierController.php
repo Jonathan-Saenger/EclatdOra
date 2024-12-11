00c199780dcc185997aa4ba2d70264cb0f7e9cff
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-12-11 23:28:43 +0100

    PANIER > Implémentation du panier

---
+++ src/Controller/PanierController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\EvenementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use App\Form\EmailInscriptionType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\EmailInscription;
+use App\Entity\Temoignage;
+use App\Form\TemoignageType;
+use App\Repository\TemoignageRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mime\Email;
+
+class PanierController extends AbstractController
+{
+    #[Route('/panier', name: 'app_panier')]
+    public function index(EvenementRepository $EvenementRepository, TemoignageRepository $temoignageRepository, Request $request, MailerInterface $mailer, EntityManagerInterface $entityManager): Response
+    {
+        $EmailInscription = new EmailInscription();
+        $formEmail = $this->createForm(EmailInscriptionType::class, $EmailInscription);
+        $formEmail->handleRequest($request);
+        if ($formEmail->isSubmitted() && $formEmail->isValid()) {
+
+            $data = $formEmail->getData();
+            $email = $data->getEmail();
+
+            $email = (new Email())
+                ->from($email)
+                ->to('cedric.eclatdora@gmail.com')
+                ->subject('Demande d\'inscription à la newsletter')
+                ->html("<p>Bonjour Cédric ! Tu as reçu une inscription à la newletter. Voici le mail du nouvel inscrit ". $email ." ");
+
+            $mailer->send($email);
+
+            $entityManager->persist($EmailInscription);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Merci ! Votre demande d\'inscription à la newsletter a bien été prise en compte !');
+            return $this->redirectToRoute('app_home');
+        }
+
+        $temoignages = new Temoignage();
+        $temoignageForm = $this->createForm(TemoignageType::class, $temoignages);
+        $temoignageForm->handleRequest($request);
+
+        if ($temoignageForm->isSubmitted() && $temoignageForm->isValid()) {
+
+            $data = $temoignageForm->getData();
+            $nom = $data->getNom();
+            $commentaire = $data->getCommentaire();
+            $email = $data->getEmail();
+
+            $email = (new Email())
+                ->from($email)
+                ->to('cedric.eclatdora@gmail.com')
+                ->subject('Nouveau commentaire d\'un client')
+                ->html("<p>Bonjour Cédric ! Tu as reçu un nouveau commentaire de ". $nom .". Rends toi dans ton espace pour le valider et le publier. Voici le commentaire : <br>
+               <cite> ". $commentaire ." </cite>. ");
+
+            $mailer->send($email);
+
+            $entityManager->persist($temoignages);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Merci pour votre témoignage ! Il sera publié après validation.');
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('panier/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'evenements' => $EvenementRepository->findBy([], ['createAt' => 'DESC'], 1),
+            'temoignageForm' => $temoignageForm->createView(),
+            'temoignages' => $temoignageRepository->findBy(['estValide' => '1']),
+            'formEmail' => $formEmail->createView(),
+        ]);
+    }
+
+}
